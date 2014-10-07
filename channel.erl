b7c794f7a7a6a50c64da5953238e72134df1435b
delta-one <patrick@malkin.de>
2014-10-07 18:00:27 +0200

    Klarar alla tests, men är ff långsamt.

--- channel.erl
+++ channel.erl
@@ -24,14 +24,27 @@ loop(St, {leave, Pid}) ->
 loop(St, {message, Pid, Nick, Channel, Msg}) ->
     case (lists:member(Pid, St#channel_st.clients)) of
 	true ->
-	    {lists:foreach(fun(ClientPID) -> genserver:request(ClientPID, {message, Channel, Nick, Msg}) end, lists:delete(Pid, St#channel_st.clients)), St};
+	    Others = lists:delete(Pid, St#channel_st.clients),
+	    spawn(fun() -> sendMessage(Others, Nick, Channel, Msg) end), 
+	    {ok, St};
+	    %spawn(?MODULE,fun(ClientPID) -> genserver:request(ClientPID, {message, Channel, Nick, Msg}) end,lists:delete(Pid, St#channel_st.clients)),
+	    %Result = lists:foreach(fun(ClientPID) -> spawn (genserver:request(ClientPID, {message, Channel, Nick, Msg})) end, lists:delete(Pid, St#channel_st.clients)),
+	    %{Result, St};
 	false ->
 	    {{error, user_not_joined, "You are not a member of this channel"}, St}
     end;
 
-
 loop(St, _Msg) -> 
       {ok, St}.
+    
+    
+sendMessage([Receiver|Restclients], Nick, Channel, Msg) ->
+    spawn( fun() -> genserver:request(Receiver, {message, Channel, Nick, Msg}) end ),
+    sendMessage(Restclients, Nick, Channel, Msg);
+    
+sendMessage([], Nick, Channel, Msg) ->
+    do_nothing.
+
 
 
 initial_state(_Name) ->
