e59e830c4b7a4a5a4bb9761eeb9ff57a09bf4ded
Cenny Davidsson <cenny@me.com>
2014-10-09 11:03:08 +0200

    Added lab 4.

---
+++ Lab4/channel.erl
@@ -0,0 +1,55 @@
+-module(channel).
+-export([loop/2, initial_state/1]).
+
+-include_lib("./defs.hrl").
+
+%%%%%%%%%%%%%%
+%%% Join
+%%%%%%%%%%%%%%
+loop(St, {join, Pid}) ->
+    case (lists:member(Pid, St#channel_st.clients)) of
+		true -> 
+	 	   {{error, user_already_joined, "You are already a member of this channel."}, St};
+		false ->
+		    {ok, St#channel_st { clients = St#channel_st.clients ++ [Pid] } }
+    end;
+  
+%%%%%%%%%%%%%%%
+%%%% Leave
+%%%%%%%%%%%%%%%
+loop(St, {leave, Pid}) ->
+    case (lists:member(Pid, St#channel_st.clients)) of
+		true ->
+	   		{ok, St#channel_st { clients = lists:delete(Pid, St#channel_st.clients)}};
+		false ->
+	   		{{error, user_not_joined, "You are not a member of this channel."}, St}
+    end;
+  
+%%%%%%%%%%%%%%%%%%%%%
+%%%% Incoming message
+%%%%%%%%%%%%%%%%%%%%%
+loop(St, {message, Pid, Nick, Channel, Msg}) ->
+%   io:format("~n~p~n",[Pid]),
+	io:format(""),
+    case (lists:member(Pid, St#channel_st.clients)) of
+		true ->
+	    	spawn(fun() -> sendMessage(lists:delete(Pid, St#channel_st.clients), Nick, Channel, Msg) end), 
+	    	{ok, St};
+		false ->
+	    	{{error, user_not_joined, "You are not a member of this channel"}, St}
+    end;
+    
+loop(St, _Msg) ->
+	{ok, St}.    
+
+sendMessage(Clients, Nick, Channel, Msg) ->
+	case Clients of
+		[Client| Rest] ->
+			spawn( fun() -> genserver:request(Client, {message, Channel, Nick, Msg}) end),
+    		sendMessage(Rest, Nick, Channel, Msg);
+    	[] ->
+    		done
+	end.
+    
+initial_state(_Name) ->
+    #channel_st{ name = _Name}.
